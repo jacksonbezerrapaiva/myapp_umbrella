03fecf0dc9a0798aa418c6afbecf671180a8142e
Jackson Paiva <jacksonpaiva@ext.cvccorp.com.br>
2019-06-18 10:16:35 -0300

    Example mock, test.

--- apps/my_app/test/my_app_test.exs
+++ apps/my_app/test/my_app_test.exs
@@ -1,8 +1,15 @@
 defmodule MyAppTest do
   use ExUnit.Case
   doctest MyApp
+  import Mock
 
   test "greets the world" do
     assert MyApp.hello() == :world
+  end
+
+  test_with_mock "test_name", HTTPotion,
+    [get: fn(_url) -> "<html></html>" end] do
+    HTTPotion.get("http://example.com")
+    assert_called HTTPotion.get("http://example.com")
   end
 end
